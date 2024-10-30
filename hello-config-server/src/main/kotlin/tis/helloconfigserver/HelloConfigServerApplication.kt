27477550f8bfee486cc3e2b42a10cb3b1a2ed9e9
이건창 <92219795+this-is-spear@users.noreply.github.com>
2024-10-30 14:26:12 +0900

    init

--- hello-config-server/src/main/kotlin/tis/helloconfigserver/HelloConfigServerApplication.kt
+++ hello-config-server/src/main/kotlin/tis/helloconfigserver/HelloConfigServerApplication.kt
@@ -2,7 +2,9 @@ package tis.helloconfigserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
+@EnableConfigServer
 @SpringBootApplication
 class HelloConfigServerApplication
 
